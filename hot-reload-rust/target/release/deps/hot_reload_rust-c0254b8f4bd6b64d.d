32101a04c6af7e8ac81d850b93e4b49e348338e3
apinanyogaratnam <apinan@ontopical.com>
2023-07-07 11:14:57 -0400

    chore: rebuild project

---
+++ hot-reload-rust/target/release/deps/hot_reload_rust-c0254b8f4bd6b64d.d
@@ -0,0 +1,5 @@
+/Users/apinanyogaratnam/Desktop/projects/hot-reload/hot-reload-rust/target/release/deps/hot_reload_rust-c0254b8f4bd6b64d: src/main.rs
+
+/Users/apinanyogaratnam/Desktop/projects/hot-reload/hot-reload-rust/target/release/deps/hot_reload_rust-c0254b8f4bd6b64d.d: src/main.rs
+
+src/main.rs:
